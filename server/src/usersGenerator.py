d7998b32069da912008f92d674a8614448966284
Matej Harabin <matejharabin@kosickaakademia.sk>
2022-11-30 21:38:12 +0100

    More names for more test accounts

--- server/src/usersGenerator.py
+++ server/src/usersGenerator.py
@@ -5,9 +5,13 @@ def xd():
     users = []
 
     names = ["Marian", "Gustáv", "Linda", "Linda", "Hubert", "Karol", "Imrich", "Števo",
-             "Rastislav", "Arpád", "Bohuslava"]
+             "Rastislav", "Arpád", "Bohuslava", "Miloš", "Emil", "Gejza", "Gašpar", "Blažej", "Erik", "Erika", "Lívia",
+             "Vlasta", "Dorota", "Viktor", "Alexander", "Eleonóra", "Albín", "Zlatica", "Miroslav", "Benjamín",
+             "Marián", "Albert", "Zoltán", "Richard", "Hugo", "Izidor", "Fedor", "Justina", "Marek", "Juraj", "Elvin",
+             "Slavomír", "Lea","Jaroslav", "Florián", "Gizela", "Svetozár", "Vilma", "Norbert", "Robert", "Alojz"]
+
     passwords = ["123456", "123456789", "qwerty", "aa12345678", "martin", "heslo", "martinko",
-                 "veronika", "password"]
+                 "veronika", "password", "killer", "monika", "000000", "dominika"]
 
     userAdmin = {
         "id": 0,
@@ -17,7 +21,7 @@ def xd():
     }
     users.append(userAdmin)
 
-    for i in range(5):
+    for i in range(25):
         user = {
             "id": i+1,
             "username": names[random.randrange(0, len(names))],
