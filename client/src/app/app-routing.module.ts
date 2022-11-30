66b0119556c43317b5cf7e957f9414c110a2612a
Jakub Kováč <jakub.kovac@kosickaakademia.sk>
2022-11-30 10:40:57 +0100

    added basic routing and other components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {AddVehicleComponent} from "./add-vehicle/add-vehicle.component";
+import {RemoveVehicleComponent} from "./remove-vehicle/remove-vehicle.component";
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'addVehicle', component: AddVehicleComponent},
+  {path: 'removeVehicle', component: RemoveVehicleComponent},
   {path: '**', component: HomeComponent}
 ];
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,18 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
-import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatDialogModule} from "@angular/material/dialog";
+import { AddVehicleComponent } from './add-vehicle/add-vehicle.component';
+import { RemoveVehicleComponent } from './remove-vehicle/remove-vehicle.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AddVehicleComponent,
+    RemoveVehicleComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
+
+    MatToolbarModule,
+    MatDialogModule,
+
     AppRoutingModule,
     BrowserAnimationsModule
   ],
