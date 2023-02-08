4e9c8fcffca847dc60440b68dc2470e5761f553e
Matej Harabin <matejharabin@kosickaakademia.sk>
2023-02-08 03:21:26 +0100

    sidliskovský sen, auta maju nove fotografie

--- server/src/init_cars.py
+++ server/src/init_cars.py
@@ -20,7 +20,7 @@ carList: list = [
     "vehicleModel": "F-150",
     "vehicleBrand": "FORD",
     "licensePlate": "FODR15",
-    "imageUrl": "https://d2cdo4blch85n8.cloudfront.net/wp-content/uploads/2017/11/1969-%E2%80%9DDickmobile%E2%80%9D-Art-Car-by-Steve-Paige-Featured-image-672x372.jpg",
+    "imageUrl": "https://www.rvtravel.com/wp-content/uploads/2020/08/fordcrash-696x522.jpg",
     "owner": names[random.randrange(0, len(names))]
 },
 {
@@ -28,7 +28,7 @@ carList: list = [
     "vehicleModel": "Mark II",
     "vehicleBrand": "TOYOTA",
     "licensePlate": "TYOTA23",
-    "imageUrl": "https://d2cdo4blch85n8.cloudfront.net/wp-content/uploads/2017/11/1969-%E2%80%9DDickmobile%E2%80%9D-Art-Car-by-Steve-Paige-Featured-image-672x372.jpg",
+    "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/f/ff/1998-2000_Toyota_Mark_II.jpg",
     "owner": names[random.randrange(0, len(names))]
 },
 {
@@ -36,7 +36,7 @@ carList: list = [
     "vehicleModel": "CIVIC ek9(Trhač asfaltu)",
     "vehicleBrand": "HONDA",
     "licensePlate": "HNDA67",
-    "imageUrl": "https://d2cdo4blch85n8.cloudfront.net/wp-content/uploads/2017/11/1969-%E2%80%9DDickmobile%E2%80%9D-Art-Car-by-Steve-Paige-Featured-image-672x372.jpg",
+    "imageUrl": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/00-1988-honda-civic-wagovan-in-colorado-junkyard-photo-by-murilee-martin-1657766923.jpg",
     "owner": names[random.randrange(0, len(names))]
 },
 {
@@ -44,7 +44,7 @@ carList: list = [
     "vehicleModel": "e36 (Sidliskový sen)",
     "vehicleBrand": "BMW",
     "licensePlate": "BMW12",
-    "imageUrl": "https://d2cdo4blch85n8.cloudfront.net/wp-content/uploads/2017/11/1969-%E2%80%9DDickmobile%E2%80%9D-Art-Car-by-Steve-Paige-Featured-image-672x372.jpg",
+    "imageUrl": "https://i.pinimg.com/736x/c7/c2/6a/c7c26af5b86535d0ee0d12f5f90f5717.jpg",
     "owner": names[random.randrange(0, len(names))]
 },
 {
@@ -52,7 +52,7 @@ carList: list = [
     "vehicleModel": "ŽTR32",
     "vehicleBrand": "NISSAN",
     "licensePlate": "NIS56",
-    "imageUrl": "https://d2cdo4blch85n8.cloudfront.net/wp-content/uploads/2017/11/1969-%E2%80%9DDickmobile%E2%80%9D-Art-Car-by-Steve-Paige-Featured-image-672x372.jpg",
+    "imageUrl": "https://i.pinimg.com/originals/9c/36/d1/9c36d1f112a38914695b86722551b40d.jpg",
     "owner": names[random.randrange(0, len(names))]
 }
 ]
